f4f2c14749918a26167720f60edefb5cfbe98986
Ikram Hakimi <ikramhakimi@gmail.com>
2019-08-08 16:33:26 +0800

    Updated git ignore

---
+++ packages/form/Gruntfile.js
@@ -0,0 +1,78 @@
+module.exports = function( grunt ) {
+
+    // Project configuration.
+    grunt.initConfig( {
+
+        clean: {
+            'dist': 'dist'
+        },
+
+        sass: {
+            build: {
+                files: {
+                    'dist/form.css': [
+                        'src/form.scss'
+                    ]
+                }
+            },
+            compressed: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'dist/form.min.css': [
+                        'src/form.scss'
+                    ]
+                }
+            }
+        },
+
+        watch: {
+            css: {
+                files: [ '**/*.{sass,scss}' ],
+                tasks: [ 'sass', 'notify:css' ],
+                options: {
+                    spawn: false,
+                    livereload: true,
+                    interrupt: true
+                },
+            }
+        },
+
+        notify: {
+            ping: {
+                options:{
+                    title: 'Nitro UI',
+                    message: 'PONG!'
+                }
+            },
+            watch: {
+                options:{
+                    title: 'Nitro UI',
+                    message: 'Watching...'
+                }
+            },
+            css: {
+                options:{
+                    title: 'Nitro UI',
+                    message: 'CSS Ready!'
+                }
+            },
+            js: {
+                options:{
+                    title: 'Nitro UI',
+                    message: 'JS Ready!'
+                }
+            }
+        }
+    } );
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks( 'grunt-contrib-clean' );
+    grunt.loadNpmTasks( 'grunt-contrib-sass' );
+    grunt.loadNpmTasks( 'grunt-contrib-watch' );
+    grunt.loadNpmTasks( 'grunt-notify' );
+
+    // Default task(s).
+    grunt.registerTask( 'default', [ 'clean', 'sass', 'notify:css' ] );
+};
