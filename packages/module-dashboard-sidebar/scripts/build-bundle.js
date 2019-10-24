ca00ed9891a0689c0cbdf4762900b4ade38c254c
Syaiful Shah Zinan <i.works@live.com>
2019-10-24 15:11:46 +0800

    Add Rollup and Bash Build Script for Module Sidebar & various fixes for Package

---
+++ packages/module-dashboard-sidebar/scripts/build-bundle.js
@@ -0,0 +1,34 @@
+var fs = require( 'fs' );
+var path = require( 'path' );
+var browserify = require( 'browserify' );
+
+var base = path.resolve( __dirname, '..' );
+var src = path.resolve( base, 'src' );
+var dist = path.resolve( base, 'dist' );
+
+// console.log( 'base = ' + base );
+// console.log( 'src = ' + src );
+// console.log( 'dist = ' + dist );
+
+var outputPathTmp = 'build-temp';
+var outputPath = dist + '/js';
+
+// console.log( 'outputPath = ' + outputPath );
+// console.log( 'outputPathTmp = ' + outputPathTmp );
+// console.log( 'dirExist = ' + fs.existsSync( outputPathTmp ) );
+
+if ( !fs.existsSync( outputPathTmp ) ) {
+    fs.mkdirSync( outputPathTmp );
+}
+
+browserify( src + '/js/sidebar.js' )
+  .transform( 'babelify', {
+      presets: [
+          '@babel/preset-env'
+      ],
+      sourceType: 'module',
+      global: true
+      // ignore: [ /\/node_modules\/(?!bootstrap\/)/ ]
+  } )
+  .bundle()
+  .pipe( fs.createWriteStream( outputPathTmp + '/sidebar-bundle.js' ) );
