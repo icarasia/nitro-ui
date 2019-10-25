fb5baf8102c955b14b1cd01872fb9d8cee36be32
Zul Azman <zul.azman@icarasia.com>
2019-10-25 16:56:05 +0800

    updated utility component package path and removed grunt

--- packages/utility-flex/scripts/build.sh
+++ packages/utility-flex/scripts/build.sh
@@ -3,7 +3,7 @@ set -e
 
 NITRO_VERSION=$(node -p "require('./package.json').version")
 PACKAGE_NAME=$(node -p "require('./package.json').name")
-FILE_NAME="utility-flex"
+FILE_NAME="flex"
 
 echo "Building ${PACKAGE_NAME} ${NITRO_VERSION}"
 echo ''
@@ -14,8 +14,8 @@ rm -rf dist
 echo 'Compile SASS...'
 node-sass --include-path=node_modules \
           --source-map=dist/$FILE_NAME.css.map \
-          src/$FILE_NAME.scss \
-          dist/$FILE_NAME.css
+          src/scss/$FILE_NAME.scss \
+          dist/css/$FILE_NAME.css
 echo 'Done.'
 echo ''
 
@@ -23,8 +23,8 @@ echo 'Compile SASS (Compress version)...'
 node-sass --include-path=node_modules \
           --output-style=compressed \
           --source-map=dist/$FILE_NAME.css.min.map \
-          src/$FILE_NAME.scss \
-          dist/$FILE_NAME.min.css
+          src/scss/$FILE_NAME.scss \
+          dist/css/$FILE_NAME.min.css
 echo 'Done.'
 echo ''
 
