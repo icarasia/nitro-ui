15e8ea3e3499fc398c3c45be8ee4c36a4412dfcb
Zul Azman <zul.azman@icarasia.com>
2019-11-01 16:30:42 +0800

    added new component toast

---
+++ packages/component-toast/src/js/toast.js
@@ -0,0 +1,13 @@
+/**
+ * Extending Bootstrap 4 Alert for Nitro UI Component Alert
+ *
+ * @package @nitro-ui/component-alert
+ * @since 1.0.0-alpha.0
+ * @author Syaiful Shah Zinan <i.works@live.com>
+ */
+
+import $ from 'jquery';
+import Toast from "bootstrap/js/src/toast";
+import Util from "bootstrap/js/src/util";
+
+$('.c-toast').toast('show')
